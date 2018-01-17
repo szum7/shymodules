6f04779c49353027ba3441d2adf6ef958b3c6f43
Szőcs Áron <szocs.aa@gmail.com>
2018-01-17 10:54:52 +0100

    users 1.0.0.1

--- user/mysql/users.sql
+++ user/mysql/users.sql
@@ -1,23 +1,24 @@
 -- ****************
 -- version-flexible 
--- v1.0.0.0
+-- v1.0.0.1
 -- ****************
 CREATE TABLE users (
+    -- id
     id                      BIGINT NOT NULL AUTO_INCREMENT,
-
+    -- login: username
     username                VARCHAR(255), -- could use username for login
     email                   VARCHAR(255), -- could use email for login
-
+    -- login: password
     password                VARCHAR(255) NOT NULL,
     salt                    VARCHAR(255),
-
+    -- additional fields
     icon                    VARCHAR(255),
-
+    -- default fields
     created_by              BIGINT,
     created_date            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_date           TIMESTAMP NOT NULL,
     last_login_date         TIMESTAMP,  
-  
+    -- restrictions
     PRIMARY KEY(id),
     FOREIGN KEY(created_by) REFERENCES users (id)
 ) ENGINE=InnoDB;
@@ -28,21 +29,22 @@ CREATE TABLE users (
 -- v1.0.0.0
 -- ****************
 CREATE TABLE users (
+    -- id
     id                      BIGINT NOT NULL AUTO_INCREMENT,
-
+    -- login: username
     username                VARCHAR(255) NOT NULL, -- for login
     email                   VARCHAR(255), -- email is not for login
-
+    -- login: password
     password                VARCHAR(255) NOT NULL,
     salt                    VARCHAR(255),
-
+    -- additional fields
     icon                    VARCHAR(255),
-
+    -- default fields
     created_by              BIGINT,
     created_date            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_date           TIMESTAMP NOT NULL,
     last_login_date         TIMESTAMP,  
-  
+    -- restrictions
     PRIMARY KEY(id),
     FOREIGN KEY(created_by) REFERENCES users (id)
 ) ENGINE=InnoDB;
